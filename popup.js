30b4f0b5803d88a62eb1b744def919072525aff1
Matouš Hýbl <hyblmatous@gmail.com>
2014-12-05 23:55:24 +0100

    Reformat code, add loading from cookie, add substitution time decisions

--- popup.js
+++ popup.js
@@ -1,53 +1,85 @@
 var classes = ["spA", "spB", "3A", "3B", "okA", "okB", "4A", "4B"];
 var classId = ["69", "70"];
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("marks").addEventListener("click", onMarksClick);
-  document.getElementById("substitution").addEventListener("click", onSubstitutionClick);
-  document.getElementById("timetable").addEventListener("click", onTimetableClick);
-  document.getElementById("moodle").addEventListener("click", onMoodleClick);
-  document.getElementById("canteen").addEventListener("click", onCanteenClick);
-  document.getElementById("web").addEventListener("click", onWebClick);
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("marks").addEventListener("click", onMarksClick);
+    document.getElementById("substitution").addEventListener("click", onSubstitutionClick);
+    document.getElementById("timetable").addEventListener("click", onTimetableClick);
+    document.getElementById("moodle").addEventListener("click", onMoodleClick);
+    document.getElementById("canteen").addEventListener("click", onCanteenClick);
+    document.getElementById("web").addEventListener("click", onWebClick);
 
     var select = document.getElementById("classSelect");
-    classes.forEach(function(object){
+    classes.forEach(function (object) {
         select.add(new Option(object));
     });
 
-    select.addEventListener("change", function changed(){
-        // FIXME save current id to a cookie
+    select.addEventListener("change", function changed() {
+        document.cookie = "id=" + classId[select.selectedIndex];
     });
 });
 
 function onMarksClick() {
-    chrome.tabs.create({ 'url': "http://www.gymkyjov.cz/isas/prubezna-klasifikace.php"});
+    chrome.tabs.create({
+        'url': "http://www.gymkyjov.cz/isas/prubezna-klasifikace.php"
+    });
 }
 
 function onSubstitutionClick() {
     var date = new Date();
-    // FIXME decide according to current date
-    date.setDate(date.getDate() + 1);
 
-    var select = document.getElementById("classSelect");
-    var url = "http://www.gymkyjov.cz/isas/suplovani.php?zobraz=tridy-1&suplovani="+classId[select.selectedIndex]+"&rezim=den&datum=" + date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
-    chrome.tabs.create({ 'url': url});
+    if (date.getHours() >= 17 || date.getDay() > 5 || date.getDay() == 0) {
+        var toAdd = 0;
+        switch (date.getDay()) {
+        case 5:
+            toAdd = 3;
+            break;
+        case 6:
+            toAdd = 2;
+            break;
+        default:
+            toAdd = 1;
+            break;
+        }
+        date.setDate(date.getDate() + toAdd);
+    }
+
+    var url = "http://www.gymkyjov.cz/isas/suplovani.php?zobraz=tridy-1&suplovani=" + getIdFromCookie() + "&rezim=den&datum=" + date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+    chrome.tabs.create({
+        'url': url
+    });
 }
 
 function onTimetableClick() {
-    // FIXME load from cookie
-    var select = document.getElementById("classSelect");
-    var url = "http://www.gymkyjov.cz/isas/rozvrh-hodin.php?zobraz=tridy-1&rozvrh=" + classId[select.selectedIndex];
-    chrome.tabs.create({ 'url': url});
+    var url = "http://www.gymkyjov.cz/isas/rozvrh-hodin.php?zobraz=tridy-1&rozvrh=" + getIdFromCookie();
+    chrome.tabs.create({
+        'url': url
+    });
 }
 
 function onMoodleClick() {
-    chrome.tabs.create({ 'url': "http://www.gymkyjov.cz/moodle"});
+    chrome.tabs.create({
+        'url': "http://www.gymkyjov.cz/moodle"
+    });
 }
 
 function onCanteenClick() {
-    chrome.tabs.create({ 'url': "http://www.gymkyjov.cz:8082"});
+    chrome.tabs.create({
+        'url': "http://www.gymkyjov.cz:8082"
+    });
 }
 
 function onWebClick() {
-    chrome.tabs.create({ 'url': "http://www.gymkyjov.cz/"});
+    chrome.tabs.create({
+        'url': "http://www.gymkyjov.cz/"
+    });
+}
+
+function getIdFromCookie() {
+    var arrayOfCookies = document.cookie.split('=');
+    if (arrayOfCookies.length < 2) {
+        alert("You have not set a class, please set it.");
+        return "69";
+    }
+    return arrayOfCookies[1];
 }
