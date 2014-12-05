018228d98436f9ac29704a3740f7d95c60f36f84
Matouš Hýbl <hyblmatous@gmail.com>
2014-12-05 23:28:57 +0100

    Add basic class select, add basic timetable and substitution funcionality

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+var classes = ["spA", "spB", "3A", "3B", "okA", "okB", "4A", "4B"];
+var classId = ["69", "70"];
+
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("marks").addEventListener("click", onMarksClick);
   document.getElementById("substitution").addEventListener("click", onSubstitutionClick);
@@ -5,6 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("moodle").addEventListener("click", onMoodleClick);
   document.getElementById("canteen").addEventListener("click", onCanteenClick);
   document.getElementById("web").addEventListener("click", onWebClick);
+
+    var select = document.getElementById("classSelect");
+    classes.forEach(function(object){
+        select.add(new Option(object));
+    });
+
+    select.addEventListener("change", function changed(){
+        // FIXME save current id to a cookie
+    });
 });
 
 function onMarksClick() {
@@ -13,11 +25,19 @@ function onMarksClick() {
 
 function onSubstitutionClick() {
     var date = new Date();
-    chrome.tabs.create({ 'url': "http://www.gymkyjov.cz/isas/prubezna-klasifikace.php"});
+    // FIXME decide according to current date
+    date.setDate(date.getDate() + 1);
+
+    var select = document.getElementById("classSelect");
+    var url = "http://www.gymkyjov.cz/isas/suplovani.php?zobraz=tridy-1&suplovani="+classId[select.selectedIndex]+"&rezim=den&datum=" + date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+    chrome.tabs.create({ 'url': url});
 }
 
 function onTimetableClick() {
-    chrome.tabs.create({ 'url': "http://www.gymkyjov.cz/isas/prubezna-klasifikace.php"});
+    // FIXME load from cookie
+    var select = document.getElementById("classSelect");
+    var url = "http://www.gymkyjov.cz/isas/rozvrh-hodin.php?zobraz=tridy-1&rozvrh=" + classId[select.selectedIndex];
+    chrome.tabs.create({ 'url': url});
 }
 
 function onMoodleClick() {
